9d45a2ed60689c53ed63ba127b2a884db6588117
Ishaan Agarwal <ishaanagarwal>
2017-03-05 20:55:50 -0500

    SerialEnv to be implemented

---
+++ src/iRobot/SerialEnvironment.java
@@ -0,0 +1,63 @@
+package iRobot;
+
+
+public class SerialEnvironment implements Environment {
+  
+
+  @Override
+  public double readLeftIR() {
+
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public double readRightIR() {
+
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public double readFrontIR() {
+
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int readIMU() {
+
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int readLeftTacho() {
+
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int readRightTacho() {
+
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public void setMotors(int left, int right) {
+
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void resetTachoCounts() {
+
+    // TODO Auto-generated method stub
+    
+  }
+
+}
