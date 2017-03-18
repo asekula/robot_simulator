11a13f689ea249af04a502f4a9d5de9144119827
asekula <alex@Alexs-MacBook-Air.local>
2017-03-18 16:41:13 -0400

    filled in serialcommrunner class

--- src/iRobot/SerialCommRunner.java
+++ src/iRobot/SerialCommRunner.java
@@ -1,13 +1,42 @@
 package iRobot;
 
+/*
+ * The main of the codebase. Runs the tests. Contains a Brain, Buffer, and
+ * Emulator object.
+ * 
+ * Note: The code below doesn't use multithreading. This class is really just an
+ * outline for what the code will be when we run it on the robot.
+ */
+
 public class SerialCommRunner {
 
 	public static void main(String[] args) {
+		TwoWaySerialComm comm = new TwoWaySerialComm();
 		try {
-			(new TwoWaySerialComm()).connect("COM3");
+			comm.connect("COM3");
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return;
 		}
+
+		Environment environment = comm;
+		DataBuffer buffer = new DataBuffer(environment);
+
+		RobotData robotData = buffer.calibrate();
+		Brain brain = new Brain(robotData);
+
+		SensorData sensorData;
+		MotorData motorData;
+
+		do {
+			sensorData = buffer.getSensorData(); // SensorData may contain noisy
+													// data.
+			motorData = brain.computeMotorData(sensorData);
+			buffer.moveRobotMotors(motorData);
+			// Q: Should wait between iterations to save computation/have more
+			// accurate data? Tacho counts may be too low if we call
+			// getSensorData too quickly.
+			// (maybe arduino is slow anyways, in which case no need)
+		} while (!brain.isFinished());
 	}
 }
