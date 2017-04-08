53429abb5b9e598b52eabecac55f0c9c37c36170
asekula <alex@Alexs-MacBook-Air.local>
2017-04-08 14:42:47 -0400

    added controller loop in serialTest

--- src/iRobot/SerialTest.java
+++ src/iRobot/SerialTest.java
@@ -14,30 +14,29 @@ import gnu.io.SerialPort;
 import gnu.io.SerialPortEvent;
 import gnu.io.SerialPortEventListener;
 
-
 public class SerialTest implements SerialPortEventListener, Environment {
 	SerialPort serialPort;
 	double frontIR, leftIR, rightIR;
-  double orientation;
-  int leftTacho, rightTacho;
-  PrintWriter pw;
-  
-        /** The port we're normally going to use. */
-	private static final String PORT_NAMES[] = { 
-			"/dev/tty.usbserial-A9007UX1", // Mac OS X
-                        "/dev/ttyACM0", // Raspberry Pi
+	double orientation;
+	int leftTacho, rightTacho;
+	PrintWriter pw;
+
+	/** The port we're normally going to use. */
+	private static final String PORT_NAMES[] = {"/dev/tty.usbserial-A9007UX1", // Mac
+																				// OS
+																				// X
+			"/dev/ttyACM0", // Raspberry Pi
 			"/dev/ttyUSB0", // Linux
 			"COM3", // Windows
 	};
 	/**
-	* A BufferedReader which will be fed by a InputStreamReader 
-	* converting the bytes into characters 
-	* making the displayed results codepage independent
-	*/
+	 * A BufferedReader which will be fed by a InputStreamReader converting the
+	 * bytes into characters making the displayed results codepage independent
+	 */
 	BufferedReader input;
 	/** The output stream to the port */
 	private OutputStream output;
-	
+
 	BufferedWriter bw;
 
 	/** Milliseconds to block while waiting for port open */
@@ -46,16 +45,18 @@ public class SerialTest implements SerialPortEventListener, Environment {
 	private static final int DATA_RATE = 9600;
 
 	public void initialize() {
-                // the next line is for Raspberry Pi and 
-                // gets us into the while loop and was suggested here was suggested http://www.raspberrypi.org/phpBB3/viewtopic.php?f=81&t=32186
-                System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM0");
+		// the next line is for Raspberry Pi and
+		// gets us into the while loop and was suggested here was suggested
+		// http://www.raspberrypi.org/phpBB3/viewtopic.php?f=81&t=32186
+		System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM0");
 
 		CommPortIdentifier portId = null;
 		Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
 
-		//First, Find an instance of serial port as set in PORT_NAMES.
+		// First, Find an instance of serial port as set in PORT_NAMES.
 		while (portEnum.hasMoreElements()) {
-			CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+			CommPortIdentifier currPortId = (CommPortIdentifier) portEnum
+					.nextElement();
 			for (String portName : PORT_NAMES) {
 				if (currPortId.getName().equals(portName)) {
 					portId = currPortId;
@@ -74,16 +75,15 @@ public class SerialTest implements SerialPortEventListener, Environment {
 					TIME_OUT);
 
 			// set port parameters
-			serialPort.setSerialPortParams(DATA_RATE,
-					SerialPort.DATABITS_8,
-					SerialPort.STOPBITS_1,
-					SerialPort.PARITY_NONE);
+			serialPort.setSerialPortParams(DATA_RATE, SerialPort.DATABITS_8,
+					SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
 
 			// open the streams
-			input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+			input = new BufferedReader(
+					new InputStreamReader(serialPort.getInputStream()));
 			output = serialPort.getOutputStream();
 			// pw = new PrintWriter(output);
-			
+
 			bw = new BufferedWriter(new OutputStreamWriter(output));
 
 			// add event listeners
@@ -95,13 +95,19 @@ public class SerialTest implements SerialPortEventListener, Environment {
 	}
 
 	/**
-	 * This should be called when you stop using the port.
-	 * This will prevent port locking on platforms like Linux.
+	 * This should be called when you stop using the port. This will prevent
+	 * port locking on platforms like Linux.
 	 */
 	public synchronized void close() {
 		if (serialPort != null) {
 			serialPort.removeEventListener();
 			serialPort.close();
+			try {
+				bw.close();
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
@@ -111,153 +117,173 @@ public class SerialTest implements SerialPortEventListener, Environment {
 	public synchronized void serialEvent(SerialPortEvent oEvent) {
 		if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
 			try {
-			  
-			  String line;
-	      while ((line = input.readLine()) != null) {
-	        char c = line.charAt(0);
-	        switch (c) {
 
-	          case 'l' :
+				String line;
+				while ((line = input.readLine()) != null) {
+					char c = line.charAt(0);
+					switch (c) {
 
-	            leftIR = Double.parseDouble(line.substring(1));
+						case 'l' :
 
-	            break;
+							leftIR = Double.parseDouble(line.substring(1));
 
-	          case 'r' :
-	            rightIR = Double.parseDouble(line.substring(1));
-	            break;
+							break;
 
-	          case 'f' :
-	            frontIR = Double.parseDouble(line.substring(1));
-	            break;
+						case 'r' :
+							rightIR = Double.parseDouble(line.substring(1));
+							break;
 
-	          case 'i' :
-	            orientation = Double.parseDouble(line.substring(1));
-	            break;
+						case 'f' :
+							frontIR = Double.parseDouble(line.substring(1));
+							break;
 
-	          case 't' :
-	            if (line.substring(1, 2).equalsIgnoreCase("l")) {
-	              leftTacho = Integer.parseInt(line.substring(2));
-	            } else {
-	              rightTacho = Integer.parseInt(line.substring(2));
-	            }
+						case 'i' :
+							orientation = Double.parseDouble(line.substring(1));
+							break;
 
-	            break;
-	        }
+						case 't' :
+							if (line.substring(1, 2).equalsIgnoreCase("l")) {
+								leftTacho = Integer.parseInt(line.substring(2));
+							} else {
+								rightTacho = Integer
+										.parseInt(line.substring(2));
+							}
 
-	      }	
+							break;
+					}
+
+				}
 			} catch (Exception e) {
 				System.err.println(e.toString());
 			}
 		}
-		// Ignore all the other eventTypes, but you should consider the other ones.
-	
-	
-	
+		// Ignore all the other eventTypes, but you should consider the other
+		// ones.
+
 	}
 
 	public static void main(String[] args) throws Exception {
 		SerialTest main = new SerialTest();
 		main.initialize();
-		Thread t=new Thread() {
+		Thread t = new Thread() {
 			public void run() {
-				//the following line will keep this app alive for 1000 seconds,
-				//waiting for events to occur and responding to them (printing incoming messages to console).
-				try {Thread.sleep(1000000);} catch (InterruptedException ie) {}
+				// the following line will keep this app alive for 1000 seconds,
+				// waiting for events to occur and responding to them (printing
+				// incoming messages to console).
+				try {
+					Thread.sleep(1000000);
+				} catch (InterruptedException ie) {
+				}
 			}
 		};
 		t.start();
 		Thread.sleep(3000);
 		System.out.println("Started");
-//		main.bw.write("100 100 1");
-//		main.bw.flush();
-		
-		main.setMotors(100,100);
-		
-		Thread.sleep(3000);
-		System.out.println("Round 2");
-		
-//		main.bw.write("-100 -100 1");
-//		main.bw.flush();
-		  main.setMotors(-100, -100);
-		
-		Thread.sleep(3000);
-		System.out.println("Round 2");
-		
-//		main.bw.write("0 0 1");
-//		main.bw.flush();
-		  
-		  main.setMotors(0, 0);
-		
-		  System.out.println("Reads -");
-		  System.out.println("Left IR "+main.readLeftIR());
-      System.out.println("Right IR "+main.readRightIR());
-      System.out.println("Front IR "+main.readFrontIR());
+		// main.bw.write("100 100 1");
+		// main.bw.flush();
 
-		  
+		main.setMotors(100, 100);
+
+		Thread.sleep(3000);
+		System.out.println("Round 2");
+
+		// main.bw.write("-100 -100 1");
+		// main.bw.flush();
+		main.setMotors(-100, -100);
+
+		Thread.sleep(3000);
+		System.out.println("Round 2");
+
+		// main.bw.write("0 0 1");
+		// main.bw.flush();
+
+		main.setMotors(0, 0);
+
+		System.out.println("Reads -");
+		System.out.println("Left IR " + main.readLeftIR());
+		System.out.println("Right IR " + main.readRightIR());
+		System.out.println("Front IR " + main.readFrontIR());
+
+		System.out.println("Starting controller.");
+
+		SensorData sensorData;
+		MotorData motorData;
+		Controller controller = new GoStraightController();
+		// Change this to change the controller used.
+
+		DataBuffer buffer = new DataBuffer(main);
+		do {
+			sensorData = buffer.getSensorData();
+			System.out.println("Sensor data: " + sensorData);
+			motorData = controller.computeMotorData(sensorData);
+			System.out.println("Motor data: " + motorData);
+			buffer.moveRobotMotors(motorData);
+		} while (!controller.isFinished());
+
+		t.join();
+		main.close();
 	}
 
+	@Override
+	public double readLeftIR() {
 
-  @Override
-  public double readLeftIR() {
+		// TODO Auto-generated method stub
+		return leftIR;
+	}
 
-    // TODO Auto-generated method stub
-    return leftIR;
-  }
+	@Override
+	public double readRightIR() {
 
-  @Override
-  public double readRightIR() {
+		// TODO Auto-generated method stub
+		return rightIR;
+	}
 
-    // TODO Auto-generated method stub
-    return rightIR;
-  }
+	@Override
+	public double readFrontIR() {
 
-  @Override
-  public double readFrontIR() {
+		// TODO Auto-generated method stub
+		return frontIR;
+	}
 
-    // TODO Auto-generated method stub
-    return frontIR;
-  }
+	@Override
+	public int readIMU() {
 
-  @Override
-  public int readIMU() {
+		// TODO Auto-generated method stub
+		return (int) orientation;
+	}
 
-    // TODO Auto-generated method stub
-    return (int) orientation;
-  }
+	@Override
+	public int readLeftTacho() {
 
-  @Override
-  public int readLeftTacho() {
+		// TODO Auto-generated method stub
+		return leftTacho;
+	}
 
-    // TODO Auto-generated method stub
-    return leftTacho;
-  }
+	@Override
+	public int readRightTacho() {
 
-  @Override
-  public int readRightTacho() {
+		// TODO Auto-generated method stub
+		return rightTacho;
+	}
 
-    // TODO Auto-generated method stub
-    return rightTacho;
-  }
-  @Override
-  public void setMotors(int left, int right) {
+	@Override
+	public void setMotors(int left, int right) {
 
-   // pw.println(left + " " + right + " " + 1);
-    try {
-      bw.write(""+left+" "+right+" "+1);
-      bw.flush();
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-   
-    
-  }
+		// pw.println(left + " " + right + " " + 1);
+		try {
+			bw.write("" + left + " " + right + " " + 1);
+			bw.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-  @Override
-  public void resetTachoCounts() {
+	}
 
-    // TODO Auto-generated method stub
-    
-  }
+	@Override
+	public void resetTachoCounts() {
+
+		// TODO Auto-generated method stub
+		// TODO: ?
+	}
 }
